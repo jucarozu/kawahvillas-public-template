d45e0e684ed00758f2e46bf79744007fc8c1941f
Juan Rojas <juanc.1909@gmail.com>
2021-05-01 01:45:25 -0500

    Add sign-in and sign-up features and CDNJS

---
+++ js/sign-in.js
@@ -0,0 +1,16 @@
+document.addEventListener('tabby', function (event) {
+	const tab = event.target;
+	const content = event.detail.content;
+
+  if (window.matchMedia('(min-width: 1024px)').matches) {
+    if (content.id == "kawahvillas-auth-sign-in") {
+      $("#kawahvillas-auth-sign-in-slogan").css("display", "flex");
+      $("#kawahvillas-auth-sign-up-slogan").hide();
+      $("#kawahvillas-auth-sign-in .email").focus();
+    } else {
+      $("#kawahvillas-auth-sign-in-slogan").hide();
+      $("#kawahvillas-auth-sign-up-slogan").css("display", "flex");
+      $("#kawahvillas-auth-sign-up .name").focus();
+    }
+  }
+}, false);
--- js/utilities/tabs.js
+++ js/utilities/tabs.js
@@ -1,16 +1,1 @@
 const tabs = new Tabby('[data-tabs]');
-
-document.addEventListener('tabby', function (event) {
-	const tab = event.target;
-	const content = event.detail.content;
-
-  if (window.matchMedia('(min-width: 1024px)').matches) {
-    if (content.id == "kawahvillas-auth-sign-in") {
-      $("#kawahvillas-auth-sign-in-slogan").css("display", "flex");
-      $("#kawahvillas-auth-sign-up-slogan").hide();
-    } else {
-      $("#kawahvillas-auth-sign-in-slogan").hide();
-      $("#kawahvillas-auth-sign-up-slogan").css("display", "flex");
-    }
-  }
-}, false);
