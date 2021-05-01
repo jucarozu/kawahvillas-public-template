2ed94873f77ace2e399c4f2d8235fd3c000e2214
Juan Rojas <juanc.1909@gmail.com>
2021-05-01 03:49:10 -0500

    Add sign-in and sign-up features and CDNJS

--- js/auth.js
+++ js/auth.js
@@ -1,6 +1,6 @@
 document.addEventListener('tabby', function (event) {
-	const tab = event.target;
-	const content = event.detail.content;
+  const tab = event.target;
+  const content = event.detail.content;
 
   if (window.matchMedia('(min-width: 1024px)').matches) {
     if (content.id == "kawahvillas-auth-sign-in") {
